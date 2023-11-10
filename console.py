e7daf9d55d88eaa76b9d0cf99e324404042c9a7f
qurriahSam <mugirethe1@gmail.com>
2023-11-10 16:05:36 +0300

    feat: creates class from console using create command

--- console.py
+++ console.py
@@ -1,5 +1,48 @@
 #!usr/bin/python3
 import cmd
+import re
+from shlex import split
+
+import models
+from models.base_model import BaseModel
+
+CLASSES = [
+    "BaseModel"
+]
+
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
+
+def check_args(args):
+    """checks if args is valid
+    Args:
+        args (str): the string containing the arguments passed to a command
+    Returns:
+        Error message if args is None or not a valid class, else the arguments
+    """
+    arg_list = parse(args)
+
+    if len(arg_list) == 0:
+        print("** class name missing **")
+    elif arg_list[0] not in CLASSES:
+        print("** class doesn't exist **")
+    else:
+        return arg_list
 
 class HBNBCommand(cmd.Cmd):
     """The class that implements the console
@@ -7,6 +50,7 @@ class HBNBCommand(cmd.Cmd):
     """
 
     prompt = "(hbnb) "
+    storage = models.storage
         
     def do_EOF(self, argv):
         """EOF signal to exit program"""
@@ -16,6 +60,19 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, argv):
         """Exits the console"""
         return True
+
+    def emptyline(self):
+        """ Do nothing on empty input line """
+        pass
+
+    def do_create(self, argv):
+        """ Creates a new instance of class """
+        args = check_args(argv)
+        if args:
+            print(eval(args[0])().id)
+            self.storage.save()
+
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
