c75669048f79d3d5784cc1bb9f2cfb20b9c7ad67
qurriahSam <mugirethe1@gmail.com>
2023-11-11 12:37:15 +0300

    feat: enable serialization and decerialization of new classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,11 +3,14 @@
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
 
-classes = {
-    "BaseModel": BaseModel,
-    "User": User
-           }
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 class FileStorage:
