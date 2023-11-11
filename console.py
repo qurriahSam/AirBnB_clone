c055759f7d027964fdcdf37dc2a8f338071ba8d1
qurriahSam <mugirethe1@gmail.com>
2023-11-11 12:38:40 +0300

    feat: enable show, create, destroy, update all with new classes

--- console.py
+++ console.py
@@ -4,10 +4,23 @@ import re
 from shlex import split
 
 import models
+from models.base_model import BaseModel
+from models.user import User
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.state import State
+from models.review import Review
 
+# A global constant since both functions within and outside uses it.
 CLASSES = [
     "BaseModel",
-    "User"
+    "User",
+    "City",
+    "Place",
+    "State",
+    "Amenity",
+    "Review"
 ]
 
 def parse(arg):
